ff968d6f923877f1d285b466aaed625f1e3cb3aa
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 09:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-// Remove duplicates from the array
-const uniqueArr = [...new Set(arr)];
-
-// Sort the array in descending order
-uniqueArr.sort((a, b) => b - a);
-
-if (uniqueArr.length <= 1) {
-  return -Infinity;
+//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length <= 1) {
+    return -Infinity;
+  }
+  
+  // Remove duplicates from the array
+  const uniqueArr = [...new Set(arr)];
+  
+  // Sort the array in descending order
+  uniqueArr.sort((a, b) => b - a);
+  
+  if (uniqueArr.length <= 1) {
+    return -Infinity;
+  }
+  
+  return uniqueArr[1];
 }
-
-return uniqueArr[1];
