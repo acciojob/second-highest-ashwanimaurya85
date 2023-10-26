6594225943f62c95db325a937af3c569a3b69463
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 09:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-//your JS code here. If required.
-function secondHighest(arr) {
-  if (arr.length <= 1) {
-    return -Infinity;
-  }
-  
-  // Remove duplicates from the array
-  const uniqueArr = [...new Set(arr)];
-  
-  // Sort the array in descending order
-  uniqueArr.sort((a, b) => b - a);
-  
-  if (uniqueArr.length <= 1) {
-    return -Infinity;
-  }
-  
-  return uniqueArr[1];
+// Remove duplicates from the array
+const uniqueArr = [...new Set(arr)];
+
+// Sort the array in descending order
+uniqueArr.sort((a, b) => b - a);
+
+if (uniqueArr.length <= 1) {
+  return -Infinity;
 }
 
-// Test the function with an example array
-const arr = [5, 10, 3, 8, 5, 2];
-const secondHighestElement = secondHighest(arr);
-console.log(`The second-highest element is: ${secondHighestElement}`);
+return uniqueArr[1];
